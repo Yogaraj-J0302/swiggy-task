32e8e5149afd4cd6bea8a3ecd39579164dc7cafa
Yogaraj <yogu0302@gmail.com>
2022-12-09 06:06:22 -0800

    Mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import React from "react";
 import { HomePage } from "./Components/HomePage";
 import { Route, Switch, useHistory } from "react-router-dom";
 import "./Components/HomePage.css";
@@ -11,92 +10,168 @@ import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import SupportIcon from "@mui/icons-material/Support";
 import LoginIcon from "@mui/icons-material/Login";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import Badge from "@mui/material/Badge";
 import HomeIcon from "@mui/icons-material/Home";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 export default function App() {
+  const [mobileOpen, setMobileOpen] = useState(false);
   const history = useHistory();
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const navItems = [
+    {
+      item: (
+        <span className="navBtn">
+          <HomeIcon /> Home
+        </span>
+      ),
+      event: () => history.push("/"),
+    },
+    {
+      item: (
+        <span className="navBtn">
+          <SearchIcon /> Search
+        </span>
+      ),
+      event: () => history.push("/search"),
+    },
+    {
+      item: (
+        <span className="navBtn">
+          <LocalOfferIcon /> Offers
+        </span>
+      ),
+      event: () => history.push("/offers"),
+    },
+    {
+      item: (
+        <span className="navBtn">
+          <SupportIcon /> Help
+        </span>
+      ),
+      event: () => history.push("/help"),
+    },
+    {
+      item: (
+        <span className="navBtn">
+          <LoginIcon /> Sign In
+        </span>
+      ),
+      event: () => history.push("/signin"),
+    },
+    {
+      item: (
+        <span className="navBtn">
+          <AddShoppingCartIcon /> Cart
+        </span>
+      ),
+      event: () => history.push("/cart"),
+    },
+  ];
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        backgroundColor: "#cc6317",
+        textAlign: "center",
+        minHeight: "100%",
+      }}
+    >
+      <Typography variant="h5" color="primary" sx={{ my: 2, color: "#fff" }}>
+        Swiggy.com
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((e) => (
+          <ListItem key={e.item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <Button key={e.item} onClick={e.event} sx={{ color: "#fff" }}>
+                {e.item}
+              </Button>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
   return (
     <div className="container-fluid">
-      <div className="col-md-12">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+      <div className="navBar">
+        <Box sx={{ display: "flex" }}>
+          <AppBar
+            className="appBar"
+            sx={{ backgroundColor: "#000" }}
+            component="nav"
           >
-            <img
-              src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
-              alt="logo"
-              height="auto"
-              width="100"
-            />
-          </IconButton>
-
-          <Button
-            style={{ marginLeft: "auto" }}
-            onClick={() => history.push("/")}
-            color="inherit"
-          >
-            <span className="navBtn">
-              <HomeIcon />
-              Home
-            </span>
-          </Button>
-          <Button
-            sx={{ ml: 4 }}
-            onClick={() => history.push("/search")}
-            color="inherit"
-          >
-            <span className="navBtn">
-              <SearchIcon />
-              Search
-            </span>
-          </Button>
-          <Badge badgeContent={"NEW"} color="error">
-            <Button
-              sx={{ ml: 4 }}
-              onClick={() => history.push("/offers")}
-              color="inherit"
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { md: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              >
+                <img
+                  src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
+                  alt="logo"
+                  height="auto"
+                  width="100"
+                />
+              </IconButton>
+              <Box
+                sx={{
+                  marginLeft: "auto",
+                  display: { xs: "none", sm: "none", md: "block" },
+                }}
+              >
+                {navItems.map((e) => (
+                  <Button
+                    key={e.item}
+                    onClick={e.event}
+                    sx={{ color: "#fff", ml: 3 }}
+                  >
+                    {e.item}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Box component="nav">
+            <Drawer
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true,
+              }}
+              sx={{
+                display: { xs: "block", sm: "block", md: "none" },
+                "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+              }}
             >
-              <span className="navBtn">
-                <LocalOfferIcon />
-                Offers
-              </span>
-            </Button>
-          </Badge>
-          <Button
-            sx={{ ml: 4 }}
-            onClick={() => history.push("/help")}
-            color="inherit"
-          >
-            <span className="navBtn">
-              <SupportIcon />
-              Help
-            </span>
-          </Button>
-          <Button
-            sx={{ ml: 4 }}
-            onClick={() => history.push("/signin")}
-            color="inherit"
-          >
-            <span className="navBtn">
-              <LoginIcon />
-              Sign In
-            </span>
-          </Button>
-          <Button
-            sx={{ ml: 4 }}
-            onClick={() => history.push("/cart")}
-            color="inherit"
-          >
-            <span className="navBtn">
-              <AddShoppingCartIcon />
-              Cart
-            </span>
-          </Button>
-        </Toolbar>
+              {drawer}
+            </Drawer>
+          </Box>
+        </Box>
       </div>
       <Switch>
         <Route exact path="/">
